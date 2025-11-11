62b36aa0163fcdc4b6fb51b3686c27ff62f7ab0c
caio <caiorbcavalcante@gmail.com>
2025-11-11 20:15:49 -0300

    fix: Corrigindo erro no models do professor

--- core/models.py
+++ core/models.py
@@ -5,6 +5,7 @@ class Professor(models.Model):
     nome = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     especialidade = models.CharField(max_length=50)
+    ativo = models.BooleanField(default=True)
     data_criacao = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
