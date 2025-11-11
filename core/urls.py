96d6a7ca7944fe31baa7373d5725622f024f1d3c
caio <caiorbcavalcante@gmail.com>
2025-11-11 17:45:50 -0300

    fix: Correções

--- core/urls.py
+++ core/urls.py
@@ -3,8 +3,8 @@ from core.controllers import aluno_controller
 
 urlpatterns = [
     path('alunos/', aluno_controller.listar_alunos),
-    path('alunos/<init:id>', aluno_controller.listar_aluno),
+    path('alunos/<int:id>/', aluno_controller.listar_aluno),  # ← int, não init
     path('alunos/criar/', aluno_controller.criar_aluno),
-    path('alunos/atualizar/<init:id>',aluno_controller.atualizar_aluno),
-    path('alunos/deletar/<init:id>',aluno_controller.deletar_aluno),
+    path('alunos/atualizar/<int:id>/', aluno_controller.atualizar_aluno),  # ← int
+    path('alunos/deletar/<int:id>/', aluno_controller.deletar_aluno),  # ← int
 ]
