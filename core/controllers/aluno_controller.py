5610874a2abe198ad4f4d56973e0139e50191b12
Leonardo Araujo <leonardoaraujo100006@gmail.com>
2025-11-11 17:34:53 -0300

    push

--- core/controllers/aluno_controller.py
+++ core/controllers/aluno_controller.py
@@ -1,1 +1,12 @@
-f
+from django.views.decorators.csrf import csrf_exempt
+from core.repositories.aluno_repository import Aluno_repository 
+import json
+from django.http import JsonResponse
+
+@csrf_exempt
+def listar_alunos(request):
+    if request.method == 'GET':
+        alunos = Aluno_repository.listar_alunos()
+        return JsonResponse(alunos,safe=False)
+    else:
+          return JsonResponse({'erro': 'alunos não encontrados'}, status=405)
