410770173563df259391dac58cfda2fcbee385b7
Leonardo Araujo <leonardoaraujo100006@gmail.com>
2025-11-11 17:11:30 -0300

    oaikao

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,8 @@ from core.controllers import aluno_controller
 
 urlpatterns = [
     path('alunos/', aluno_controller.listar_alunos),
+    path('alunos/<init:id>', aluno_controller.listar_aluno),
     path('alunos/criar/', aluno_controller.criar_aluno),
-    path('alunos/atualizar',aluno_controller.atualizar_aluno)
+    path('alunos/atualizar/<init:id>',aluno_controller.atualizar_aluno),
+    path('alunos/deletar/<init:id>',aluno_controller.deletar_aluno),
 ]
