dac6942cd8bce8a5d78cad31fe80946c87b11bfc
caio <caiorbcavalcante@gmail.com>
2025-11-18 10:21:18 -0300

    feat: Modificando modelos para adicionar informações

--- core/models.py
+++ core/models.py
@@ -1,13 +1,32 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 
-class Professor(models.Model):
+class ProfessorManager(BaseUserManager):
+    def create_user(self, email, nome, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Email obrigatório')
+        email = self.normalize_email(email)
+        user = self.model(email=email, nome=nome, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+class Professor(AbstractBaseUser):
     nome = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     especialidade = models.CharField(max_length=50)
+    foto = models.URLField(max_length=500, blank=True, null=True, default='https://cdn-icons-png.flaticon.com/512/4140/4140047.png')
+    preco_hora = models.DecimalField(max_digits=8, decimal_places=2, default=80.00)
+    minutos_disponiveis = models.IntegerField(default=240)
+    descricao = models.TextField(blank=True, null=True)
     ativo = models.BooleanField(default=True)
     data_criacao = models.DateTimeField(auto_now_add=True)
+
+    objects = ProfessorManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['nome']
 
     class Meta:
         db_table = 'professores'                    # Nome da tabela no banco
@@ -15,9 +34,43 @@ class Professor(models.Model):
         verbose_name_plural = 'Professores'         # Nome plural no Admin
         ordering = ['nome']                         # Ordena por nome A-Z
 
-
     def __str__(self):
         return f"Prof. {self.nome} - {self.especialidade}"
+    
+
+class AlunoManager(BaseUserManager):
+    def create_user(self, email, nome, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Email obrigatório')
+        email = self.normalize_email(email)
+        user = self.model(email=email, nome=nome, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+class Aluno(AbstractBaseUser):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    foto = models.URLField(blank=True, null=True)
+    numero = models.CharField(max_length=15)
+    ativo = models.BooleanField(default=True)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+
+    objects = AlunoManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['nome']
+
+
+    class Meta:
+        db_table = 'alunos' 
+        verbose_name = 'Aluno'
+        verbose_name_plural = 'Alunos'
+        ordering = ['nome'] 
+
+    def __str__(self):
+        return self.nome
+
+
 class Agendamento(models.Model):
     # Choices para status do agendamento
     STATUS_CHOICES = [
@@ -69,22 +122,6 @@ class Agendamento(models.Model):
     def __str__(self):
         return f"{self.aluno.nome} com {self.professor.nome} - {self.data_agendamento.strftime('%d/%m/%Y %H:%M')}"
     
-
-class Aluno(models.Model):
-    nome = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    numero = models.CharField(max_length=15)
-
-    def __str__(self):
-        return self.nome
-
-    class Meta:
-        db_table = 'alunos' 
-        verbose_name = 'Aluno'
-        verbose_name_plural = 'Alunos'
-        ordering = ['nome'] 
-
-
 
 class Servico(models.Model):
     descricao = models.CharField(max_length=100)
