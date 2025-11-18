28aace0bf055f4a1d9f725f08a5e6f34c969dd95
caio <caiorbcavalcante@gmail.com>
2025-11-18 15:17:41 -0300

    fix: Ajeitando AppRoutes

--- educaon-frontend/src/routes/appRoutes.jsx
+++ educaon-frontend/src/routes/appRoutes.jsx
@@ -2,14 +2,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/home";
 import Login from "../pages/login";
-
+import ProfessorDashboard from "../pages/ProfessorDashboard";
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login/>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/professor-dashboard" element={<ProfessorDashboard />} />
       </Routes>
     </BrowserRouter>
   );
