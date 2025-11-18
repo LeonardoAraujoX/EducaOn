b2af041b643f8c732b8bf43c3943a19252cb4603
caio <caiorbcavalcante@gmail.com>
2025-11-18 09:30:55 -0300

    feat: Colocando a api usando axios e criando protótipo estático de dashboard de professores

--- educaon-frontend/src/routes/appRoutes.jsx
+++ educaon-frontend/src/routes/appRoutes.jsx
@@ -2,6 +2,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/home";
 import Login from "../pages/login";
+import ProfessorDashboard from "../pages/ProfessorDashboard";
 
 
 export default function AppRoutes() {
@@ -10,6 +11,7 @@ export default function AppRoutes() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login/>} />
+        <Route path="/professor-dashboard" element={<ProfessorDashboard/>}/>
       </Routes>
     </BrowserRouter>
   );
